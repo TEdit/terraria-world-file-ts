9e9c4e91571afa63064e3a6c032a22f742da0ffd
Lukáš Džubák <lukas.dzubak@student.tuke.sk>
2025-11-23 23:49:34 +0100

    wip saver

---
+++ src/types.ts
@@ -0,0 +1,110 @@
+export enum GameMode {
+  NormalMode,
+  ExpertMode,
+  MasterMode,
+  CreativeMode,
+}
+
+export type Tile = {
+  blockId?: number
+  frameX?: number
+  frameY?: number
+  blockColor?: number
+  wallId?: number
+  wallColor?: number
+  liquidAmount?: number
+  liquidType?: Liquid
+  wireRed?: boolean
+  wireBlue?: boolean
+  wireGreen?: boolean
+  slope?: Slope
+  actuator?: boolean
+  actuated?: boolean
+  wireYellow?: boolean
+  invisibleBlock?: boolean
+  invisibleWall?: boolean
+  fullBrightBlock?: boolean
+  fullBrightWall?: boolean
+}
+
+export enum Slope {
+  Half = 1,
+  TR,
+  TL,
+  BR,
+  BL,
+}
+
+export enum Liquid {
+  Water = 1,
+  Lava,
+  Honey,
+  Shimmer,
+}
+
+export type Position = {
+  x: number
+  y: number
+}
+
+export type Item = {
+  stack: number
+  id: number
+  prefix: number
+}
+
+export type ItemSlot = Item | null
+
+export type Chest = {
+  position: Position
+  name?: string
+  items?: ItemSlot[]
+}
+
+export type Sign = {
+  text: string
+  position: Position
+}
+
+export interface Pillar {
+  id: number
+  position: Position
+}
+
+export interface TownNPC extends Pillar {
+  name: string
+  homeless: boolean
+  homePosition: Position
+  variationIndex?: number
+  shimmered?: boolean
+}
+
+export type TownRoom = {
+  NPCId: number
+  position: Position
+}
+
+export type WeightedPressurePlate = {
+  position: {
+    x: number
+    y: number
+  }
+}
+
+export enum CreativePowerType {
+  FreezeTime = 0,
+  StartDayImmediately,
+  StartNoonImmediately,
+  StartNightImmediately,
+  StartMidnightImmediately,
+  GodModePower,
+  ModifyWindDirectionAndStrength,
+  ModifyRainPower,
+  ModifyTimeRate = 8,
+  FreezeRainPower = 9,
+  FreezeWindDirectionAndStrength = 10,
+  FarPlacementRangePower,
+  DifficultySliderPower = 12,
+  StopBiomeSpreadPower = 13,
+  SpawnRateSliderPerPlayerPower,
+}
